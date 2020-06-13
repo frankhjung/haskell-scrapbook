077f8e2b1322247996f4e89ad35184a3ad774c76
Frank Jung <frankhjung@linux.com>
2020-06-13 18:00:13 +1000

    use reverse for last

--- mylast.hs
+++ mylast.hs
@@ -13,17 +13,20 @@ Play with last and penultimate of a list.
 
 module MyLast (main) where
 
-import           Data.Maybe (Maybe (..), isNothing)
+import           Control.Arrow ((>>>))
+import           Data.Maybe    (Maybe (..), isNothing)
+
 
 -- | Get last element of a list.
 myLast :: [a] -> a
-myLast []     = error "No last element"
-myLast [a]    = a
-myLast (_:as) = myLast as
+myLast = head . myRev1
+-- myLast []     = error "No last element"
+-- myLast [a]    = a
+-- myLast (_:as) = myLast as
 
 test1 :: IO ()
 test1 =
-  let as = [1,2,3]
+  let as = [1..5]
   in print $ last as == myLast as
 
 test2 :: IO ()
@@ -31,6 +34,7 @@ test2 =
   let as = [4]
   in print $ last as == myLast as
 
+-- | Get second to last element of a list.
 penultimate :: [a] -> Maybe a
 penultimate []       = Nothing
 penultimate [_]      = Nothing
@@ -39,18 +43,36 @@ penultimate (_:a:as) = penultimate (a:as)
 
 test3 :: IO ()
 test3 =
-    let as = [1,2]
-    in print $ Just 1 == penultimate as
-
-test4 :: IO ()
-test4 =
     let as = [1]
     in print $ isNothing (penultimate as)
 
+test4 :: IO ()
+test4 =
+    let as = [1,2]
+    in print $ Just 1 == penultimate as
+
 test5 :: IO ()
 test5 =
-    let as = [1,2,3,4,5]
-    in print $ Just 4 == penultimate as
+    let as = [1..5]
+    in print $ Just (((!! 1) . reverse) as) == penultimate as
+
+-- | Reverse a list, version 1.
+myRev1 :: [a] -> [a]
+myRev1 = foldl (flip (:)) []
+
+-- | Reverse a list, version 2.
+myRev2 :: [a] -> [a]
+myRev2 as = foldr (\ a s -> (a :) >>> s) id as []
+
+test6 :: IO ()
+test6 =
+    let as = [1..5]
+    in print $ last as == (head . myRev1) as
+
+test7 :: IO ()
+test7 =
+    let as = [1..5]
+    in print $ myRev1 as == myRev2 as
 
 -- | Test myLast
 main :: IO ()
@@ -60,4 +82,6 @@ main =
   >> test3
   >> test4
   >> test5
+  >> test6
+  >> test7
 
