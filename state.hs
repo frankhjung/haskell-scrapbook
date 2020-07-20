c1777d269e393f817866cff5be0c6e30dfd39676
Frank Jung <frankhjung@linux.com>
2020-07-20 19:30:28 +1000

    update state to uncurry function sequence

--- state.hs
+++ state.hs
@@ -7,9 +7,10 @@ Description : Deriving the State Monad
 Copyright   : © Frank Jung, 2020
 License     : GPL-3
 
-From <https://williamyaoh.com/posts/2020-07-12-deriving-state-monad.html Deriving the State Monad by William Yaoh>
+From
+<https://williamyaoh.com/posts/2020-07-12-deriving-state-monad.html Deriving the State Monad by William Yaoh>
 
->>> echo -e "1 2 3 4 5 6 \n" | runhaskell state.hs
+>>> echo 1 2 3 4 5 6 | runhaskell state.hs
 (2,["1","2","3","4","5","6"])
 
 -}
@@ -20,13 +21,11 @@ module State (main) where
 reverseWithCount :: Int -> [a] -> (Int, [a])
 reverseWithCount count list = (count + 1, reverse list)
 
--- | Reverse the list twice giving back original but incremented count.
+-- | Reverse a list twice giving back original but incremented call count.
 reverseTwiceWithCount :: Int -> [a] -> (Int, [a])
-reverseTwiceWithCount count list = (c', a')
-  where
-    (c, a) = reverseWithCount count list
-    (c', a') = reverseWithCount c a
+reverseTwiceWithCount count list = uncurry reverseWithCount $ reverseWithCount count list
 
 -- Reverse list of contents from command line.
 main :: IO ()
 main = interact $ show . reverseTwiceWithCount 0 . words
+
