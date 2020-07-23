8993e3d4ecb8979fe809983ac4ba104f69ec8a80
Frank Jung <frankhjung@linux.com>
2020-07-23 23:02:01 +1000

    more on state monad

--- state.hs
+++ state.hs
@@ -10,34 +10,80 @@ License     : GPL-3
 From
 <https://williamyaoh.com/posts/2020-07-12-deriving-state-monad.html Deriving the State Monad by William Yaoh>
 
->>> echo 1 2 3 4 5 6 | runhaskell state.hs
-(2,["1","2","3","4","5","6"])
+> echo 1 2 3 4 5 6 | runhaskell state.hs
+> (2,["1","2","3","4","5","6"])
 
 -}
 
-module State (main) where
+{-# LANGUAGE TupleSections #-}
 
-data State s a = State { runState :: s -> (s, a) }
+module State (State(..), reverseWithCount, reverseTwiceWithCount ) where
 
--- define show
-instance (Show s, Show a) => Show (State s a) where
-  show (State s a) = "(" ++ show s ++ ", " ++ show a ++ ")"
+-- | State type is defined with a stateful function that takes takes in
+-- a current state and returns an updated state along with its "normal"
+-- return value.
+--
+-- Here 'runState' is of type:
+--
+-- > runState :: State s a -> s -> (s, a)
+--
+newtype State s a = State { runState :: s -> (s, a) }
 
--- define Functor
--- define Applicative
--- define Monad
+-- state :: (s -> (a, s)) -> State s a
+
+-- TODO define a State class with get, put and modify operations.
+
+-- Returns current state.
+get :: State s s
+get = State (\s -> (s,s))
+
+-- Replace current state with the given value. Returns unit.
+put :: s -> State s ()
+put s = State (const (s, ()))
+
+-- Update current state using the given function
+modify :: (s -> s) -> State s ()
+modify f = get >>= put . f
+
+-- TODO define show instance
+-- Show count.
+-- showCount :: State Int [String] -> String
+-- showCount s = do
+--   (s', a') <- get
+--   show (s', a')
+
+-- Functor
+instance Functor (State s) where
+  fmap f (State stateFx) =
+    State (\s -> let (s', fx) = stateFx s
+                 in (s', f fx))
+
+-- Applicative
+instance Applicative (State s) where
+  pure x = State (,x)
+  (<*>) (State stateFx) (State nextFx) =
+    State (\s -> let (s', fx) = stateFx s
+                     (s'', ffx) = nextFx s'
+                 in (s'', fx ffx))
+
+-- Monad
+instance Monad (State s) where
+  return = pure
+  (>>=) (State stateFx) nextFx =
+    State (\s -> let (s', fx) = stateFx s
+                 in runState (nextFx fx) s')
 
 -- | Reverse a list, and increase a count of function calls.
-reverseWithCount :: [a] -> State Int [a]
+reverseWithCount :: [String] -> State Int [String]
 reverseWithCount list = State (\s -> (s + 1, reverse list))
 
 -- | Reverse a list twice giving back original but incremented call count.
-reverseTwiceWithCount :: [a] -> State Int [a]
+reverseTwiceWithCount :: [String] -> State Int [String]
 reverseTwiceWithCount list = reverseWithCount list
                              >>= reverseWithCount
-                             >>= (\list1 -> State (\s -> (s +1, list1)))
+                             >>= (\ls -> State (\s -> (s+1, ls)))
 
 -- Reverse list of contents from command line.
-main :: IO ()
-main = interact $ show . reverseTwiceWithCount . words
+-- main :: IO ()
+-- main = interact $ show . reverseTwiceWithCount . words
 
