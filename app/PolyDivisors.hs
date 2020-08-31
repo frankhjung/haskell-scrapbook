dd5ef5c8fde6d93d38be4d763de48d802bf9823d
Frank Jung <frankhjung@linux.com>
2020-08-31 11:33:36 +1000

    refactor from recursion to fold

--- app/PolyDivisors.hs
+++ app/PolyDivisors.hs
@@ -23,6 +23,23 @@ I first read about puzzle in Matt Parkers book,
 >>> $ stack exec polydivisors 123456789
 [381654729]
 
+== Refactored
+
+Refactor to use a fold:
+
+>>> x = 123456789
+>>> n = length (show x)
+
+>>> λ> map (\p -> x `div` 10^p ) [0..n-1]
+[123456789,12345678,1234567,123456,12345,1234,123,12,1]
+
+>>> λ> f x = let n = length (show x) in x `mod` n == 0
+>>> λ> f 12
+True
+
+>>> λ> foldr (\x -> (&&) (f x)) True xs
+False
+
 -}
 
 module Main (main) where
@@ -32,25 +49,29 @@ import           System.Environment (getArgs)
 
 -- | Find poly-divisor of input string.
 findPolyDiv :: Int -> [Int]
-findPolyDiv x = filterPolyMod (length (show x)) (perms x)
+findPolyDiv = filterPolyMod . perms
 
--- | Filter numbers modulo @n ... 1@.
-filterPolyMod :: Int -> [Int] -> [Int]
-filterPolyMod n = filter (isPolyMod n)
+-- | Filter numbers modulo n..1
+filterPolyMod :: [Int] -> [Int]
+filterPolyMod = filter isPolyMod
 
--- | Test number is modulo @n ... 1@.
-isPolyMod :: Int -> Int -> Bool
-isPolyMod n x = all (==0) (polyMod n x)
-  where
-    polyMod :: Int -> Int -> [Int]
-    polyMod n' x'
-      | n' == 1    = [0]
-      | otherwise = x' `mod` n' : polyMod (n' - 1) (x' `div` 10)
+-- | Check if poly-divisor of input.
+isPolyMod :: Int -> Bool
+isPolyMod x =
+  let
+    -- Is value modulus of length?
+    -- ie. Is 123 mod 3 = 0?
+    isModLen :: Int -> Bool
+    isModLen a = a `mod` length (show a) == 0
+    -- list of x's reduced by factor of 10 for length of x as a string
+    -- eg. 123 gives [123, 12, 1]
+    xs = map (\p -> x `div` 10^p ) [0..length (show x) - 1]
+  in foldr ((&&) . isModLen) True xs
 
 -- | Permutations of argument.
 perms :: Int -> [Int]
 perms xs = map read (permutations (show xs))
 
--- | Search for poly-divisors of digits @1 .. 9@.
+-- | Search for poly-divisors of digits 1..9
 main :: IO ()
 main = getArgs >>= mapM_ (print . findPolyDiv . read)
