e1485c51dd16ced7b52d9649814d983072786f12
Frank Jung <frankhjung@linux.com>
2021-06-28 16:27:34 +1000

    counter using a non-recursive method using unfoldr

--- app/Counter.hs
+++ app/Counter.hs
@@ -4,23 +4,40 @@
 
 Use 'fix' to recurse.
 
-From
-<https://blog.sumtypeofway.com/posts/introduction-to-recursion-schemes.html Introduction to recursion schemes>
+Modified from algorithm described in
+<https://blog.sumtypeofway.com/posts/introduction-to-recursion-schemes.html Introduction to recursion schemes>.
+
+Next, build a fixed-point combinator in the type system.
+
+@
+type Y t = t (t (t (t (t ...))))
+@
+
+That is a data type @Y@ that, when given another type @f@, wraps an @f@
+whose children are of type @(Y f)@.
+
 -}
 
 module Main (main) where
 
 import           Data.Function      (fix)
+import           Data.List          (unfoldr)
 import           Numeric.Natural    (Natural)
 import           System.Environment (getArgs)
 
--- | A counter from 1 to a given natural number.
-counter :: Natural -> [Natural]
-counter n = fix (\rec xs@(x:_) -> if x > 1 then rec (x - 1:xs) else xs) [n]
+-- | A counter from 1 to a natural number, `n`.
+-- Same as @[1..n]@
+countUp :: Natural -> [Natural]
+countUp n = fix (\rec xs@(x:_) -> if x > 1 then rec (x - 1:xs) else xs) [n]
+
+-- | Count down from a natural number, `n` to 1.
+-- Same as @[n,n-1..1]@
+countDown :: Natural -> [Natural]
+countDown = unfoldr (\n -> if n == 0 then Nothing ; else Just (n, n - 1))
 
 -- | Count to a given natural number.
 main :: IO ()
 main = getArgs >>= \args -> case length args of
-  1 -> let n = (read . head) args in print (counter n)
+  1 -> let n = (read . head) args in mapM_ print [countUp n, countDown n]
   _ -> error "Usage: [number]\nCount up to a given natural number."
 
