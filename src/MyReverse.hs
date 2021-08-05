f4c0c5b9b8b344267a9d246989fd33272315788b
Frank Jung <frankhjung@linux.com>
2021-08-05 23:17:04 +1000

    update documentation for reverse

--- src/MyReverse.hs
+++ src/MyReverse.hs
@@ -19,15 +19,15 @@ myRevl = foldl (flip (:)) []
 
 -- | Reverse a list using foldr.
 myRevr :: [a] -> [a]
-myRevr as = foldr (\ a s -> (a :) >>> s) id as []
+myRevr xs = foldr (\ x acc -> (x :) >>> acc) id xs []
 
--- | Second version to reverse a list using foldr.
+-- | Reverse using recursion and reverse operation.
 myRevr2 :: [a] -> [a]
 myRevr2 = rev revOp []
   where
     rev _ acc []      = acc
     rev op acc (x:xs) = rev op (acc `op` x) xs
 
--- | Reverse operation.
+-- | Operation to place element at the beginning of a list.
 revOp :: [a] -> a -> [a]
 revOp xs x = x : xs
