f883270ce24d7930f192b354059bd4697c7e228d
Frank Jung <frankhjung@linux.com>
2022-05-06 11:20:09 +1000

    my take using recursion

---
+++ src/MyTake.hs
@@ -0,0 +1,20 @@
+{- |
+
+Module      : MyTake
+Description : Implement take
+Copyright   : © Frank Jung, 2022
+License     : GPL-3
+
+-}
+
+module MyTake (myTake) where
+
+-- | My version of take.
+-- This does not check for negative numbers.
+--
+-- >>> myTake 1 "abc"
+-- "a"
+myTake :: Int -> [a] -> [a]
+myTake n _ | n <= 0 = []
+myTake _ []         = []
+myTake n (x:xs)     = x : myTake (pred n) xs
---
+++ test/MyTakeSpec.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module MyTakeSpec (spec) where
+
+import           MyTake                (myTake)
+import           Test.Hspec            (Spec, describe)
+import           Test.Hspec.QuickCheck (prop)
+import           Test.QuickCheck       (NonNegative (..))
+
+spec :: Spec
+spec =
+  describe "myTake" $
+    prop "myTake same as preludes take" $
+      \(n :: Int, xs :: String) -> myTake n xs == take n xs
