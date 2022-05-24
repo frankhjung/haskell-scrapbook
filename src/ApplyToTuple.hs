d6928526b5d1d37eaece154ac3412ad558c2e164
Frank Jung <frankhjung@linux.com>
2022-05-24 11:38:13 +1000

    added rank n types example

---
+++ src/ApplyToTuple.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE RankNTypes #-}
+{-|
+
+Module      : ApplyToTuple
+Description : Example rank-n-type
+Copyright   : © Frank Jung, 2021
+License     : GPL-3
+
+Example rank-n-type from
+<http://sleepomeno.github.io/blog/2014/02/12/Explaining-Haskell-RankNTypes-for-all Explaining Haskell RankNTypes for all>.
+-}
+
+module ApplyToTuple (applyToTuple) where
+
+-- | Example of a rank N type. The `apply` function will apply the function
+-- to the provided input tuple.
+--
+-- >>> applyToTuple length ("foo", [1,2,3,4])
+-- (3,4)
+applyToTuple :: forall b c. (forall a. [a] -> Int) -> ([b], [c]) -> (Int, Int)
+applyToTuple f (xs, ys) = (f xs, f ys)
---
+++ test/ApplyToTupleSpec.hs
@@ -0,0 +1,14 @@
+module ApplyToTupleSpec (spec) where
+
+import           ApplyToTuple (applyToTuple)
+
+import           Test.Hspec   (Spec, describe, it, shouldBe)
+
+
+spec :: Spec
+spec =
+  describe "apply length to tuple" $
+    it "length 3 4" $ do
+      let xs = "foo" :: String
+          ys = [1..4] :: [Int]
+      applyToTuple length (xs, ys) `shouldBe` (3,4)
