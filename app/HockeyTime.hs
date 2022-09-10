735e9f871e581436061565e7dfd0d9afea1cfaee
Frank Jung <frankhjung@linux.com>
2022-09-10 17:28:50 +1000

    add hockey time logic puzzle

---
+++ app/HockeyTime.hs
@@ -0,0 +1,124 @@
+{-# LANGUAGE RecordWildCards #-}
+-- {-# LANGUAGE OverloadedRecordDot #-} -- supported in 9.2
+
+module Main (main) where
+
+{-
+
+== Solve the Hockey Time logic puzzle
+
+This year's dream team has been picked! Players from around the nation have
+been chosen to participate in a final playoff extravaganza. Your task is to
+help figure out 5 of the players who were chosen to participate, the team
+they play for, how many goals they have scored this season and the number
+on their jersey.
+
+=== Clues
+
+1. Mahoney is either the person who wears number 26 or the player from the Icers
+2. Quill doesn't wear number 26
+3. Nates scored 2 goals more than Pilgrim
+4. The player from the Red Boots scored 1 goal more than the person from the Warriors
+5. The player who wears number 25 plays for the Warriors.
+6. The player who wears number 4 scored 1 goal less than the player who wears number 26.
+7. The person with 8 goals is from the Warriors
+8. Douglas scored 1 goal more than the player who wears number 25
+9. Of the person from the Cougars and the person with 11 goals,
+   one is Quill and the other wears number 22.
+
+=== References
+
+- https://www.ahapuzzles.com/logic/logic-puzzles/hockey-time/
+- https://www.youtube.com/watch?v=bPyR1ttdE7o&t=1623s&ab_channel=Tweag
+
+-}
+
+import           Control.Monad
+import           Data.List
+import           Data.Maybe
+
+data Player = Douglas | Mahoney | Nates | Pilgrim | Quill
+              deriving (Enum, Bounded, Show, Eq)
+
+data Team = Cougars | Icers | Monsters | RedBoots | Warriors
+              deriving (Enum, Bounded, Show, Eq)
+
+players = [minBound..maxBound]
+teams = [minBound.. maxBound]
+jerseys = [4, 7, 22, 25, 26]
+goals = [8, 9, 10, 11, 12]
+
+data Assignment = MkAssignment { player :: Player, team :: Team, jersey :: Int, goal :: Int }
+                    deriving (Eq)
+
+instance Show Assignment where
+  show MkAssignment {..} = "(" ++ show player ++ ", "
+                            ++ show team ++ ", "
+                            ++ show jersey ++ ", "
+                            ++ show goal ++ ")"
+
+type Solution = [Assignment]
+
+-- | Method:
+-- - Fix the players
+-- - permute the teams
+-- - permute the jerseys
+-- - permute the goals
+-- - filter with clues
+--
+answers :: [Solution]
+answers = do
+  solution@[douglas, mahoney, nates, pilgrim, quill]
+    <- [zipWith4 MkAssignment players ts js gs
+       | ts <- permutations teams
+       , js <- permutations jerseys
+       , gs <- permutations goals]  -- has 1728000 permutations
+
+  -- (1) Mahoney is either the person who wears number 26 or the player from the Icers
+  Just icers <- [find (\p -> team p == Icers) solution]
+  guard (jersey mahoney == 26 || jersey icers == 26)
+  guard (team mahoney /= Icers)
+
+  -- (2) Quill doesn't wear number 26
+  guard (jersey quill /= 26)
+
+  -- 3. Nates scored 2 goals more than Pilgrim.
+  guard (goal nates - goal pilgrim == 2)
+
+  -- 4. The player from the Red Boots scored 1 goal more
+  -- than the person from the Warriors
+  Just redboots <- [find (\p -> team p == RedBoots) solution]
+  Just warriors <- [find (\p -> team p == Warriors) solution]
+  guard (goal redboots - goal warriors == 1)
+
+  -- 5. The player who wears number 25 plays for the Warriors.
+  Just jersey25 <- [find (\p -> jersey p == 25) solution]
+  guard (team jersey25 == Warriors)
+
+  -- 6. The player who wears number 4
+  -- scored 1 goal less than the player who wears number 26
+  Just jersey4 <- [find (\p -> jersey p == 4) solution]
+  Just jersey26 <- [find (\p -> jersey p == 26) solution]
+  guard (goal jersey4 - goal jersey26 == (-1))
+
+  -- 7. The person with 8 goals is from the Warriors
+  -- Just goals8 <- [find (\p -> goal p == 8) solution]
+  -- guard (team goals8 == Warriors)
+  guard (goal warriors == 8)
+
+  -- 8. Douglas scored 1 goal more than the player who wears number 25
+  -- Just jersey25 <- [find (\p -> jersey p == 25) solution]
+  guard (goal douglas - goal jersey25 == 1)
+
+  -- 9. Of the person from the Cougars and the person with 11 goals,
+  -- one is Quill and the other wears number 22
+  let clue9 p1 p2 = team p1 == Cougars && goal p2 == 11
+  Just jersey22 <- [find (\p -> jersey p == 22) solution]
+  guard (clue9 quill jersey22 || clue9 jersey22 quill)
+  guard (quill /= jersey22)
+
+  return solution
+
+-- | Solve the Hockey Time logic puzzle.
+main :: IO ()
+main = mapM_ print answers
