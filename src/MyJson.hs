5f1cb2d0187e9a2637251d6a88d8845bf3361405
Frank Jung <frankhjung@linux.com>
2023-04-24 16:07:11 +1000

    decode non-utf8 characters

--- src/MyJson.hs
+++ src/MyJson.hs
@@ -12,16 +12,31 @@ License     : GPL-3
 
 Decode JSON which contains special characters like '°' (ASCII decimal \176).
 
+A modified decode function to parse a ByteString containing special characters:
+
+import Data.Text.Encoding (encodeUtf8Builder)
+import Data.ByteString.Builder(toLazyByteString)
+import Data.Aeson (decode)
+
+decodeUtf8 :: FromJSON a => Text -> Maybe a
+decodeUtf8 = decode . toLazyByteString . encodeUtf8Builder
+
 -}
 
-module MyJson (MyJson (..)) where
+module MyJson ( MyJson (..)
+              , eitherDecodeUft8
+              ) where
 
-import           Data.Aeson (FromJSON, Value (..), parseJSON, (.:))
--- import           Data.ByteString.Lazy.Internal (ByteString (..))
-import           Data.Text  (Text)
--- import           Data.Text.Encoding            (decodeUtf8)
+import           Data.Aeson                    (FromJSON, Value (..),
+                                                eitherDecode, parseJSON, (.:))
+-- import           Data.ByteString.Builder (toLazyByteString)
+import           Data.ByteString.Lazy.Internal (ByteString (..))
+import           Data.Text                     (Text)
+import           Data.Text.Lazy.Encoding       (decodeLatin1, encodeUtf8)
+
 
 -- | Define a test data type.
+-- TODO parse a datetime string.
 data MyJson = MyJson
   { name       :: Text
   , identifier :: Int
@@ -34,3 +49,20 @@ instance FromJSON MyJson where
                             <*> v .: "identifier"
                             <*> v .: "modifier"
   parseJSON _          = fail "Expected an object"
+
+-- TODO write ToJSON instance.
+
+{-
+
+Data.Text.Lazy.pack :: String -> Text
+
+Data.Text.Lazy.Encoding.decodeUtf8 :: ByteString -> Text
+Data.Text.Lazy.Encoding.encodeUtf8 :: Text -> ByteString
+Data.Aeson.eitherDecode :: ByteString -> Either String a
+
+eitherDecodeUft8 = eitherDecode . toLazyByteString . encodeUtf8Builder
+-}
+
+-- | Decode a ByteString containing special characters such as '°' (ASCII decimal \176).
+eitherDecodeUft8 :: FromJSON a => ByteString -> Either String a
+eitherDecodeUft8 = eitherDecode . encodeUtf8 . decodeLatin1
--- test/MyJsonSpec.hs
+++ test/MyJsonSpec.hs
@@ -10,17 +10,25 @@ module MyJsonSpec (spec) where
 
 import           Data.Aeson                    (eitherDecode)
 import           Data.ByteString.Lazy.Internal (ByteString (..))
+
 import           Test.Hspec                    (Spec, describe, it, shouldBe)
 
-import           MyJson                        (MyJson (..))
+import           MyJson                        (MyJson (..), eitherDecodeUft8)
 
 jsonByteString :: ByteString
 jsonByteString = "{ \"name\":\"Frank\"\
                  \,\"identifier\":1\
                  \,\"modifier\":2.14}"
 
+jsonSpecialString :: ByteString
+jsonSpecialString = "{ \"name\":\"François\"\
+                    \,\"identifier\":1\
+                    \,\"modifier\":2.14}"
+
 spec :: Spec
 spec =
-  describe "test decode utf-8 input" $
-    it "returns an instance of MyJson type" $
+  describe "test decode utf-8 input" $ do
+    it "regular byte string" $
       eitherDecode jsonByteString `shouldBe` Right (MyJson "Frank" 1 2.14)
+    it "special byte string" $
+       eitherDecodeUft8 jsonSpecialString `shouldBe` Right (MyJson "François" 1 2.14)
