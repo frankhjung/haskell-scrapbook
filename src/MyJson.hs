d776b9d72ea020a7e58467bf57342074b04055cd
Frank Jung <frankhjung@linux.com>
2023-04-24 16:51:17 +1000

    encode utf8 characters

--- src/MyJson.hs
+++ src/MyJson.hs
@@ -1,7 +1,7 @@
--- {-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts   #-}
 {-# LANGUAGE OverloadedStrings  #-}
-
 
 {-|
 
@@ -12,27 +12,27 @@ License     : GPL-3
 
 Decode JSON which contains special characters like '°' (ASCII decimal \176).
 
-A modified decode function to parse a ByteString containing special characters:
+== References
 
-import Data.Text.Encoding (encodeUtf8Builder)
-import Data.ByteString.Builder(toLazyByteString)
-import Data.Aeson (decode)
-
-decodeUtf8 :: FromJSON a => Text -> Maybe a
-decodeUtf8 = decode . toLazyByteString . encodeUtf8Builder
+I found this a good guide when dealing with JSON and special characters:
+https://guide.aelve.com/haskell/aeson-cookbook-amra6lk6
 
 -}
 
 module MyJson ( MyJson (..)
-              , eitherDecodeUft8
+              , FromJSON
+              , ToJSON
+              , eitherDecodeSpecial
+              , encodeSpecial
               ) where
 
-import           Data.Aeson                    (FromJSON, Value (..),
+import           Data.Aeson                    (FromJSON, ToJSON, Value (..),
                                                 eitherDecode, parseJSON, (.:))
--- import           Data.ByteString.Builder (toLazyByteString)
+import           Data.Aeson.Text               (encodeToLazyText)
 import           Data.ByteString.Lazy.Internal (ByteString (..))
 import           Data.Text                     (Text)
 import           Data.Text.Lazy.Encoding       (decodeLatin1, encodeUtf8)
+import           GHC.Generics                  (Generic)
 
 
 -- | Define a test data type.
@@ -41,7 +41,7 @@ data MyJson = MyJson
   { name       :: Text
   , identifier :: Int
   , modifier   :: Float
-  } deriving stock (Eq, Show)
+  } deriving stock (Eq, Show, Generic)
 
 instance FromJSON MyJson where
   parseJSON (Object v) =  MyJson
@@ -50,19 +50,23 @@ instance FromJSON MyJson where
                             <*> v .: "modifier"
   parseJSON _          = fail "Expected an object"
 
--- TODO write ToJSON instance.
+-- TODO can we encode UTF-8 when encoding to JSON?
+instance ToJSON MyJson
 
-{-
+-- | Decode Special Characters.
+--
+-- Data.Text.Lazy.Encoding.decodeUtf8 :: ByteString -> Text
+-- Data.Text.Lazy.Encoding.encodeUtf8 :: Text -> ByteString
+-- Data.Aeson.eitherDecode :: ByteString -> Either String a
+--
+-- This will successfully decode a ByteString containing special characters such
+-- as '°' (ASCII decimal \176).
+eitherDecodeSpecial :: FromJSON a => ByteString -> Either String a
+eitherDecodeSpecial = eitherDecode . encodeUtf8 . decodeLatin1
 
-Data.Text.Lazy.pack :: String -> Text
-
-Data.Text.Lazy.Encoding.decodeUtf8 :: ByteString -> Text
-Data.Text.Lazy.Encoding.encodeUtf8 :: Text -> ByteString
-Data.Aeson.eitherDecode :: ByteString -> Either String a
-
-eitherDecodeUft8 = eitherDecode . toLazyByteString . encodeUtf8Builder
--}
-
--- | Decode a ByteString containing special characters such as '°' (ASCII decimal \176).
-eitherDecodeUft8 :: FromJSON a => ByteString -> Either String a
-eitherDecodeUft8 = eitherDecode . encodeUtf8 . decodeLatin1
+-- | Encode Special Characters.
+--
+-- Data.Aeson.Text.encodeToLazyText :: ToJSON a => a -> Text
+-- Data.Text.Lazy.Encoding.encodeUtf8 :: Text -> ByteString
+encodeSpecial :: ToJSON a => a -> ByteString
+encodeSpecial = encodeUtf8 . encodeToLazyText
--- test/MyJsonSpec.hs
+++ test/MyJsonSpec.hs
@@ -8,27 +8,40 @@ Can encode and decode when ByteString does not contain special characters like
 
 module MyJsonSpec (spec) where
 
-import           Data.Aeson                    (eitherDecode)
+import           Data.Aeson                    (eitherDecode, encode)
 import           Data.ByteString.Lazy.Internal (ByteString (..))
 
 import           Test.Hspec                    (Spec, describe, it, shouldBe)
 
-import           MyJson                        (MyJson (..), eitherDecodeUft8)
+import           MyJson                        (MyJson (..),
+                                                eitherDecodeSpecial,
+                                                encodeSpecial)
 
-jsonByteString :: ByteString
-jsonByteString = "{ \"name\":\"Frank\"\
-                 \,\"identifier\":1\
-                 \,\"modifier\":2.14}"
-
-jsonSpecialString :: ByteString
-jsonSpecialString = "{ \"name\":\"François\"\
+jsonRegularString :: ByteString
+jsonRegularString = "{\"name\":\"Frank\"\
                     \,\"identifier\":1\
                     \,\"modifier\":2.14}"
 
+jsonSpecialString :: ByteString
+jsonSpecialString = "{\"name\":\"François\"\
+                    \,\"identifier\":2\
+                    \,\"modifier\":3.14}"
+
+myJsonRegular :: MyJson
+myJsonRegular = MyJson "Frank" 1 2.14
+
+myJsonSpecial :: MyJson
+myJsonSpecial = MyJson "François" 2 3.14
+
 spec :: Spec
-spec =
-  describe "test decode utf-8 input" $ do
-    it "regular byte string" $
-      eitherDecode jsonByteString `shouldBe` Right (MyJson "Frank" 1 2.14)
-    it "special byte string" $
-       eitherDecodeUft8 jsonSpecialString `shouldBe` Right (MyJson "François" 1 2.14)
+spec = do
+  describe "test decode" $ do
+    it "decode regular byte string" $
+      eitherDecode jsonRegularString `shouldBe` Right myJsonRegular
+    it "decode special byte string" $
+      eitherDecodeSpecial jsonSpecialString `shouldBe` Right myJsonSpecial
+  describe "test encode" $ do
+    it "encode regular" $
+      (eitherDecode . encode) myJsonRegular `shouldBe` Right myJsonRegular
+    it "encode special" $
+      (eitherDecode . encodeSpecial) myJsonSpecial `shouldBe` Right myJsonSpecial
