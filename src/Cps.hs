ace211e100fdc2658d5a5c58bbcbb4e0f11dbf51
Frank Jung <frankhjung@linux.com>
2024-05-17 14:35:48 +1000

    update continuation passing style

--- src/Cps.hs
+++ src/Cps.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 {-|
 
 Module      : Cps
@@ -5,19 +7,34 @@ Description : Continuation passing style, examples
 Copyright   : © Frank Jung, 2020
 License     : GPL-3
 
-Example from
-<https://en.wikibooks.org/wiki/Haskell/Continuation_passing_style Wikibooks Continuation Passing Style>.
+Example from [Wikibooks Continuation Passing
+Style](https://en.wikibooks.org/wiki/Haskell/Continuation_passing_style)
 
-Uses the Cont monad from the transformers package.
+This uses the 'Cont' Monad from the
+[transformers](https://hackage.haskell.org/package/transformers) package.
 
-Using @callCC@ is better than using return as argument is a function and it
+Using 'callCC' is better than using return as argument is a function and it
 returns a suspended computation.
+
+A function of type @a -> b@ would become @a -> (b -> r) -> r@ in CPS, where
+@b -> r@ is the continuation.
 
 -}
 
-module Cps (addCont, pythagorasCont, squareCont) where
+module Cps
+  ( -- * Functions
+    addCont
+  , pythagorasCont
+  , squareCont
+  , toCPS
+  , fromCPS
+  ) where
 
 import           Control.Monad.Trans.Cont (Cont, callCC)
+
+-- | Continuation passing style (CPS) examples.
+--
+-- The following uses the 'Cont' Monad from the 'transformers' package.
 
 -- | Continuation for add function.
 addCont :: Int -> Int -> Cont r Int
@@ -33,3 +50,37 @@ pythagorasCont x y = do
     x_squared <- squareCont x
     y_squared <- squareCont y
     addCont x_squared y_squared
+
+-- | Custom continuation passing style.
+--
+-- The following example uses a custom continuation passing style (CPS) to
+-- demonstrate the general concept.
+--
+-- This converts any value into a suspended computation.
+--
+-- @flip ($) :: b -> (b -> c) -> c@
+--
+-- Longhand this is:
+-- @
+-- toCPS a = \k -> k a
+-- @
+--
+-- Or more simply as:
+-- @
+-- toCPS a k = k a
+-- @
+toCPS :: a -> (forall r. (a -> r) -> r)
+toCPS = flip ($)
+
+-- | From custom continuation.
+--
+-- Passing 'id' to the continuation function will return the value.
+--
+-- @($ id) :: ((a -> a) -> b) -> b@
+--
+-- Longhand this is:
+-- @
+-- fromCPS f = f id
+-- @
+fromCPS :: (forall r. (a -> r) -> r) -> a
+fromCPS = ($ id)
--- test/CpsSpec.hs
+++ test/CpsSpec.hs
@@ -3,14 +3,19 @@
 module CpsSpec (spec) where
 
 import           Control.Monad.Trans.Cont (runCont)
-import           Cps                      (pythagorasCont)
+import           Cps                      (fromCPS, pythagorasCont, toCPS)
 import           Test.Hspec               (Spec, describe, it, shouldBe)
 import           Test.Hspec.QuickCheck    (prop)
 
 spec :: Spec
-spec =
-  describe "continuation passing style - cps" $ do
+spec = do
+  describe "continuation passing style (cps)" $ do
     it "pythagoras 3 4 is 25" $
-      runCont (pythagorasCont 3 4) Just `shouldBe` Just 25
-    prop "pythagoras a b is a*a + b*b" $
+      runCont (pythagorasCont 3 4) id `shouldBe` 25
+    prop "pythagoras a b as Just" $
       \(a :: Int, b :: Int) -> runCont (pythagorasCont a b) Just == Just (a*a + b*b)
+    prop "pythagoras a b as String" $
+      \(a :: Int, b :: Int) -> runCont (pythagorasCont a b) show == show (a*a + b*b)
+  describe "custom cps" $
+    prop "fromCPS to id" $
+      \(a :: Int) -> fromCPS (toCPS a) `shouldBe` a
